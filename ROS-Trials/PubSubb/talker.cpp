2239c563f7d68b47f4f0f0c22276c4732d20c5ba
newb-dev-1008 <yashumale223@gmail.com>
2021-09-22 22:01:32 +0530

    Talker changes

--- ROS-Trials/PubSubb/talker.cpp
+++ ROS-Trials/PubSubb/talker.cpp
@@ -2,6 +2,13 @@
 # include "std_msgs/String.h"
 # include <sstream>
 
+using namespace std;
+using namespace ros;
+
 int main(int argc, char **argv) {
-    
+    init(argc, argv, "Talker");
+
+    NodeHandler n;
+    Publisher publisher = n.advertise<std_msgs::String>("Chatter", 1000);
+    Rate loop_rate(1.0);
 })
